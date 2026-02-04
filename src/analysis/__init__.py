dd771af5c9872ff106f3046d05d0a99a5cdaf4c4
adi1bioinfo <adi1bioinfo@gmail.com>
2026-02-04 18:44:15 +0530

    Move utils into src/analysis; add src package and utils shim

---
+++ src/analysis/__init__.py
@@ -0,0 +1,4 @@
+# analysis package - re-export utilities
+from .trajectory_loader import *
+from .analysis_functions import *
+from .visualization import *
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,23 +1,4 @@
-# utils/__init__.py
-# Import main functions for easy access
-
-from .trajectory_loader import load_trajectory, validate_files
-from .analysis_functions import (
-    calculate_rmsd_rmsf,
-    calculate_sasa,
-    calculate_hydrogen_bonds,
-)
-from .visualization import plot_rmsd, plot_rmsf, plot_sasa_heatmap
-
-__version__ = "1.0.0"
-
-__all__ = [
-    "load_trajectory",
-    "validate_files",
-    "calculate_rmsd_rmsf",
-    "calculate_sasa",
-    "calculate_hydrogen_bonds",
-    "plot_rmsd",
-    "plot_rmsf",
-    "plot_sasa_heatmap",
-]
+# shim utils package to preserve import paths
+from src.analysis.trajectory_loader import *
+from src.analysis.analysis_functions import *
+from src.analysis.visualization import *
